09cd48f0febd991d2a32f84f7af5a4a376bb4d5a
Vpx2104 <162385592+Vpx2104@users.noreply.github.com>
2024-05-21 00:50:08 +0000

    f

---
+++ gerenciamento.cpp
@@ -0,0 +1,49 @@
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+    //definir a variavel
+    char opcao;
+
+    // declarar os itens do menu 
+    cout << "1.adicionar tarefas " << endl; 
+    cout << "2.Visualizar Tarefas "<< endl;
+    cout << "3. Editar Tarefa"<< endl;
+    cout << "4. Remover Tarefa " << endl; 
+    cout << "5. Buscar Tarefa "<< endl;
+    cout << "6. Filtrar Tarefas por Status"<< endl;
+    cout << "0. sair"<< endl; 
+    cout << "escolha uma opcao" << endl;
+    cin >> opcao;
+
+    if (opcao == 1) {
+        
+    } else if (opcao == 2)
+    {
+        /* code */
+    } else if (opcao == 3)
+    {
+        /* code */
+    } else if (opcao == 4)
+    {
+        /* code */
+    } else if (opcao == 5)
+    {
+        /* code */
+    } else if (opcao == 6) {
+
+    } else if (opcao == 0){
+
+    } else {
+        
+    }
+      
+    
+    
+    
+
+
+
+    return 0;
+}
